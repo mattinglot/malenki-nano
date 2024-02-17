8c3bd1ef3b59aa3bea5ca348eded628fde4fb1e6
Matt Inglot <1845166+mattinglot@users.noreply.github.com>
2024-02-17 13:23:03 -0700

    Added docker container for compiling firmware.

---
+++ start-docker.bat
@@ -0,0 +1,4 @@
+echo Starting AVR Development Environment...
+docker-compose up -d
+echo AVR Development Environment is up and running.
+pause
---
+++ stop-docker.bat
@@ -0,0 +1,4 @@
+echo Stopping AVR Development Environment...
+docker-compose down
+echo AVR Development Environment has been stopped.
+pause
